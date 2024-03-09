cdcb0c48abd5d365fed12a44838969d0f689697c
TOM0402 <48129750+TOM0402@users.noreply.github.com>
2024-03-09 15:44:41 +0100

    aggiornamento task

--- classifica..cpp
+++ classifica..cpp
@@ -1,18 +1,5 @@
 #include "classifica.hpp"
 using namespace std;
-/*
-class Classifica {
-public:
-    void add_player();
-    void update();
-    void getClassification();
-private:
-    char classification[];
-};
-*/
-
-
-
 
 void Classifica::add_player(char name[], int score){
     cout << j.dump(4) << endl;
