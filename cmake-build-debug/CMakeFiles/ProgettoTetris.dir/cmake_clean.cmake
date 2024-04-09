db2e06487a9f5fb9981ea3fbdddc3418575e1466
TOM0402 <tommasobb@gmail.com>
2024-04-09 12:17:00 +0200

    Fix Cmake

---
+++ cmake-build-debug/CMakeFiles/ProgettoTetris.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProgettoTetris.dir/Classifica/classifica.cpp.o"
+  "CMakeFiles/ProgettoTetris.dir/Classifica/classifica.cpp.o.d"
+  "CMakeFiles/ProgettoTetris.dir/Giocatore/Giocatore.cpp.o"
+  "CMakeFiles/ProgettoTetris.dir/Giocatore/Giocatore.cpp.o.d"
+  "CMakeFiles/ProgettoTetris.dir/fileManagement/file.cpp.o"
+  "CMakeFiles/ProgettoTetris.dir/fileManagement/file.cpp.o.d"
+  "CMakeFiles/ProgettoTetris.dir/main.cpp.o"
+  "CMakeFiles/ProgettoTetris.dir/main.cpp.o.d"
+  "ProgettoTetris"
+  "ProgettoTetris.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProgettoTetris.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
