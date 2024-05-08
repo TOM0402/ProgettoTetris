3b134529244534581bc96f2f561da7d98a4f06d0
TOM0402 <tommasobb@gmail.com>
2024-05-08 13:23:21 +0200

    clear ora solo cartelle file e main

--- backup/b1/Classifica/classifica.cpp
+++
@@ -1,9 +0,0 @@
-#include "classifica.hpp"
-#include "../fileManagement/file.hpp"
-using namespace std;
-HandlerClassifica::HandlerClassifica() {
-
-}
-bool aggiungi(Giocatore g);
-Giocatore [] getClassifica();
-bool rimuovi(Giocatore g);
